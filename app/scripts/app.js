f07ee02442bfbbcdd0d408e1c0362a136ed3fb12
AGdelaX <agdelax@gmail.com>
2015-06-16 18:40:26 -0400

    added ui router and tasks to app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,6 @@
-var blocItOff = angular.module("BlocItOff", ['firebase']);
+"use strict";
+
+var blocItOff = angular.module('BlocItOff', ['firebase', 'ui.router']);
 
 blocItOff.factory('Tasks', ['$firebaseArray', function($firebaseArray, $scope){
 	var firebaseRef = new Firebase('https://shining-torch-9429.firebaseio.com/');
@@ -7,4 +9,25 @@ blocItOff.factory('Tasks', ['$firebaseArray', function($firebaseArray, $scope){
 	return {
 		tasks: tasklist
 	};
+}]);
+
+blocItOff.run(function(){
+	console.log("running");
+});
+
+blocItOff.config(function($stateProvider, $locationProvider){
+	$locationProvider.html5Mode(true);
+	// $urlRouterProvider.otherwise('/');
+
+	$stateProvider
+	.state('home', {
+		url: '/',
+		controller: 'TasklistView',
+		templateUrl: '/templates/home.html'
+	})
+});
+
+blocItOff.controller('TasklistView', ['$scope', 'Tasks', function($scope, Tasks){
+	$scope.tasklist = Tasks.tasks;
+	$scope.message = "testing";
 }]);
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var blocItOff = angular.module("BlocItOff", ['firebase']);
+"use strict";
+
+var blocItOff = angular.module('BlocItOff', ['firebase', 'ui.router']);
 
 blocItOff.factory('Tasks', ['$firebaseArray', function($firebaseArray, $scope){
 	var firebaseRef = new Firebase('https://shining-torch-9429.firebaseio.com/');
@@ -8,5 +10,26 @@ blocItOff.factory('Tasks', ['$firebaseArray', function($firebaseArray, $scope){
 	return {
 		tasks: tasklist
 	};
+}]);
+
+blocItOff.run(function(){
+	console.log("running");
+});
+
+blocItOff.config(function($stateProvider, $locationProvider){
+	$locationProvider.html5Mode(true);
+	// $urlRouterProvider.otherwise('/');
+
+	$stateProvider
+	.state('home', {
+		url: '/',
+		controller: 'TasklistView',
+		templateUrl: '/templates/home.html'
+	})
+});
+
+blocItOff.controller('TasklistView', ['$scope', 'Tasks', function($scope, Tasks){
+	$scope.tasklist = Tasks.tasks;
+	$scope.message = "testing";
 }]);
 },{}]},{},[1]);
